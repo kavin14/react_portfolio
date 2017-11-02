47f48fbd3bb075e7d2745dfde60273bfd381b28d
kavin <kavin.isaac@gmail.com>
2017-11-01 17:38:23 -0700

    Revert "Initial commit"
    This reverts commit c04aac7303f0634ddac64e4f4d666c1fccbebe61.

--- src/reducers/reducer_glify.js
+++
@@ -1,9 +0,0 @@
-export default function glifyReducer(state = [], action){
-    switch(action.type){
-        case 'SEARCH-TERM':
-            return [action.payload.data]
-         case 'GLIFY_RESET':
-            return []    
-    }
-    return state;
-}
