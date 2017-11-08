0123ef519b0b02d8b22654501079ebb177302f31
kavin <kavin.isaac@gmail.com>
2017-11-08 00:02:50 -0800

    jquery

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,29 @@
 import React,{Component} from 'react';
+import $ from 'jquery';
 import { Link } from 'react-router-dom';
 
 
 export default class HeaderSection extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            sidePaneIsVisible: false
+        }
     }
-
+    onCLickHamburgerMenu(){
+        this.setState({sidePaneIsVisible: true})
+    }
     render(){
         var headerLogoStyle = {
             marginLeft : "10px"
         }
+        let showSidePane = (this.state.sidePaneIsVisible)? {transform: "translateX(0px)"} : {transform: "translateX(-100%)"};
         return (
             <div>
             <nav>
     <div className="nav-wrapper red">
       <a href="#!" >Kavin Premkumar</a>
-      <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+      <a href="#" data-activates="mobile-demo" className="button-collapse" ><i className="material-icons">menu</i></a>
       <ul className="right hide-on-med-and-down">
         <li><Link to='/'> Home </Link></li>
                         <li><Link to='/aboutme'> Skills </Link></li>
@@ -24,7 +31,7 @@ export default class HeaderSection extends Component {
                         <li><Link to='/glify'>Gliphy-app</Link></li>   
                         <li><Link to='/todo'>ToDo-App</Link></li> 
       </ul>
-      <ul className="side-nav" id="mobile-demo">
+      <ul className="side-nav" id="mobile-demo" >
        <li><Link to='/'> Home </Link></li>
                         <li><Link to='/aboutme'> Skills </Link></li>
                         <li><Link to='/weatherApp'>Weather-App</Link></li>
